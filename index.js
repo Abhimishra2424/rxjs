5e0c039759fa98880ff7aa1021a4d868f1e944dc
Abhishek Mishra <abmishra700@gmail.com>
2022-08-11 11:18:27 +0530

    crearted pipe line

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const { Observable } = require('rxjs')
+const { map } = require('rxjs/operators')
+
+const users = {
+    data: [
+        {
+            status: "active",
+            age: 20,
+        },
+        {
+            status: "inactive",
+            age: 10,
+        },
+        {
+            status: "inactive",
+            age: 25,
+        },
+        {
+            status: "active",
+            age: 70,
+        },
+    ]
+}
 
 
 // how they get the Data so they have a subscriber to get the data 
 const observable = new Observable((subscriber) => {
-    subscriber.next(10)
-})
+    subscriber.next(users)
+}).pipe(
+    map((value) => {
+        console.log("1= Got data from Observable", value)
+        return value.data
+    }),
+    map((value) => {
+        console.log("1= Got data from first Operator", value)
+        return value
+    })
+)
 
 
 const observer = {
